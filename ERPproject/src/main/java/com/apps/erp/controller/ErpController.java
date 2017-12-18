05bba696af1ea9b9b6d588022c0e00061dad5747
baese <morentod90@gmail.com>
2017-12-18 18:40:17 +0900

    jsp

--- ERPproject/src/main/java/com/apps/erp/controller/ErpController.java
+++ ERPproject/src/main/java/com/apps/erp/controller/ErpController.java
@@ -1,5 +1,9 @@
 package com.apps.erp.controller;
 
 public class ErpController {
-	
+	public static void main(String[] args) {
+		System.out.println("branch test");
+		System.out.println("add");
+		//branch2
+	}
 }
