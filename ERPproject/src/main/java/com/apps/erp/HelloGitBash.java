2bd021b0bffd2d21ef3a8d8db010cc9f04cf0dcf
ShinJe Kim <shinjehere@gmail.com>
2017-12-19 14:36:42 +0900

    add new line

--- ERPproject/src/main/java/com/apps/erp/HelloGitBash.java
+++ ERPproject/src/main/java/com/apps/erp/HelloGitBash.java
@@ -1,1 +1,11 @@
-System.out.println("Hello Git Bash");
+package com.apps.erp;
+
+public class HelloGitBash{
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello Git Bash");
+		System.out.println("Hello Git Commit");
+	}
+
+} 
