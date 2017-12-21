f292fb342b45d01c3b057922e769e2b41373305d
Kim Young Seop <kkimm87@gmail.com>
2017-12-22 01:33:02 +0900

    수정연습

--- ERPproject/src/main/java/com/apps/erp/HelloGitBash.java
+++ ERPproject/src/main/java/com/apps/erp/HelloGitBash.java
@@ -3,7 +3,7 @@ package com.apps.erp;
 public class HelloGitBash{
 
 	public static void main(String[] args){
-		
+	/*	
 		System.out.println("Hello Git Bash");
 		System.out.println("Hello Git Commit");
 		System.out.println("branch test");
@@ -11,7 +11,7 @@ public class HelloGitBash{
 		System.out.println("system.out.println");
 		System.out.println("이게 깃하는 방법인가?");
 		String b="restful 어려워";
-		System.out.println(b);
+		System.out.println(b);*/
 	}
 
 } 
